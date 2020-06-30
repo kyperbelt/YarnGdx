726fced7536e4499dce322d96b566dd77669404e
Eliot Lash <fadookie@gmail.com>
2020-06-30 07:31:01 -0700

    add plumbing for JUnit5 Tests

---
+++ core/test/com/kyper/yarn/tests/yarn/ProgramTest.java
@@ -0,0 +1,45 @@
+package com.kyper.yarn.tests.yarn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadStrings() {
+        assertTrue(false, "Making sure JUnit Works!");
+    }
+
+    @org.junit.jupiter.api.Test
+    void registerString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dumpCode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTextForNode() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void include() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void untaggedStrings() {
+    }
+}
