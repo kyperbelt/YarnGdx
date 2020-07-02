044f84e5758053bed3a5ffaddb640f1baf4decf8
Eliot Lash <fadookie@gmail.com>
2020-07-02 07:35:22 -0700

    copy g4 files from YarnSpinner and try downgrading to antlr 4.7.1 and remove YarnSpinner submodule

--- tools/generate_compiler.sh
+++ tools/generate_compiler.sh
@@ -8,6 +8,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 BIN_DIR="$SCRIPT_PATH/tmp-bin"
 OUT_DIR="$SCRIPT_PATH/../core/src/com/kyper/yarn/compiler"
 JAR_PATH="$BIN_DIR/antlr4.jar"
+GRAMMAR_DIR="grammarFork"
 
 set +o nounset
 export CLASSPATH=".:$JAR_PATH:$CLASSPATH"
@@ -18,8 +19,8 @@ mkdir -p "$OUT_DIR"
 rm -Rf "$OUT_DIR/*"
 
 if [[ ! -f "$JAR_PATH" ]]; then
-    curl --location https://www.antlr.org/download/antlr-4.8-complete.jar -o "$JAR_PATH"
+    curl --location https://www.antlr.org/download/antlr-4.7.1-complete.jar -o "$JAR_PATH"
 fi
 
-java -Xmx500M -cp "$JAR_PATH:$CLASSPATH" org.antlr.v4.Tool -lib YarnSpinnerCSharp/YarnSpinner.Compiler/ YarnSpinnerCSharp/YarnSpinner.Compiler/*.g4 -o "$OUT_DIR" -visitor -package com.kyper.yarn.compiler -Xlog -Xexact-output-dir
+java -Xmx500M -cp "$JAR_PATH:$CLASSPATH" org.antlr.v4.Tool -lib "$GRAMMAR_DIR" "$GRAMMAR_DIR"/*.g4 -o "$OUT_DIR" -visitor -package com.kyper.yarn.compiler -Xlog -Xexact-output-dir
 
